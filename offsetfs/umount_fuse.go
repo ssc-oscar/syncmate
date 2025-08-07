a421b3c91afbca98cea2ff6a07451d06777f374c
hrz6976 <hrz6976@hotmail.com>
2025-08-07 20:56:16 +0800

    Umount with -zu

--- offsetfs/umount_fuse.go
+++ offsetfs/umount_fuse.go
@@ -6,7 +6,7 @@ package offsetfs
 import "os/exec"
 
 func UmountExec(mountpoint string) error {
-	if err := exec.Command("fusermount", "-u", mountpoint).Run(); err != nil {
+	if err := exec.Command("fusermount", "-zu", mountpoint).Run(); err != nil {
 		return err
 	}
 	return nil
--- offsetfs/umount_fuse3.go
+++ offsetfs/umount_fuse3.go
@@ -6,7 +6,7 @@ package offsetfs
 import "os/exec"
 
 func UmountExec(mountpoint string) error {
-	if err := exec.Command("fusermount3", "-u", mountpoint).Run(); err != nil {
+	if err := exec.Command("fusermount3", "-zu", mountpoint).Run(); err != nil {
 		return err
 	}
 	return nil
